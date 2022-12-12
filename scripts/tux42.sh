2e7a6a4007d43059b82c7691bdb22c43f38d5544
jsousa02 <jorgedaniel.sousa02@gmail.com>
2022-12-12 10:12:06 +0000

    fix scripts

--- scripts/tux42.sh
+++ scripts/tux42.sh
@@ -2,7 +2,7 @@
 
 ifconfig eth0 172.16.41.1/24
 route add -net 172.16.40.1/24 gw 172.16.41.253
-route add default gw 172.16.41.254/24
+route add default gw 172.16.41.254
 route -n
 echo 0 > /proc/sys/net/ipv4/conf/eth0/accept_redirects
 echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
--- scripts/tux44.sh
+++ scripts/tux44.sh
@@ -2,7 +2,7 @@
 
 ifconfig eth0 172.16.40.254/24
 ifconfig eth1 172.16.41.253/24
-route add default gw 172.16.41.254/24
+route add default gw 172.16.41.254
 route -n
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
